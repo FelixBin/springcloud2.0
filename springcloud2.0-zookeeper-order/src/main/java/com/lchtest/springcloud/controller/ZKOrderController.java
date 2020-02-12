063739ec415d02b70e4bfd68f01feab82a047005
liuch0228 <1402828263@qq.com>
2020-02-12 15:55:42 +0800

    通过DiscoveryClient获取注册中心上的服务列表信息

--- springcloud2.0-zookeeper-order/src/main/java/com/lchtest/springcloud/controller/ZKOrderController.java
+++ springcloud2.0-zookeeper-order/src/main/java/com/lchtest/springcloud/controller/ZKOrderController.java
@@ -1,8 +1,12 @@
 package com.lchtest.springcloud.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -33,4 +37,19 @@ public class ZKOrderController {
 		String memberUrl = "http://zk-member/getmember";
 		return restTemplate.getForObject(memberUrl, String.class);
 	}
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	/**
+	 * 通过DiscoveryClient获取注册中心上的服务列表信息  http://localhost:8003/getServiceInfo
+	 * {"serviceId":"zk-order","host":"windows10.microdone.cn","port":8003,"secure":false,"uri":"http://windows10.microdone.cn:8003","metadata":{},"serviceInstance":{"name":"zk-order","id":"bd4d9b0f-24dc-45bd-9a85-d99a248dd332","address":"windows10.microdone.cn","port":8003,"sslPort":null,"payload":{"id":"application-1","name":"zk-order","metadata":{}},"registrationTimeUTC":1581493274959,"serviceType":"DYNAMIC","uriSpec":{"parts":[{"value":"scheme","variable":true},{"value":"://","variable":false},{"value":"address","variable":true},{"value":":","variable":false},{"value":"port","variable":true}]},"enabled":true},"scheme":null}
+	 * @return
+	 */
+	@RequestMapping("/getServiceInfo")
+	public Object getServiceInfo() {
+		List<ServiceInstance> instances = discoveryClient.getInstances("zk-order");
+		return instances.get(0);  //服务集群情况下返回的是多个服务的集合
+	}
+	
 }
