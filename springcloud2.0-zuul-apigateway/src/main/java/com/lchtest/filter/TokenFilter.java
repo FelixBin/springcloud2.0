eb931002c31773f5031e135c3dade3fe98eef84c
liuch0228 <1402828263@qq.com>
2020-02-15 15:26:34 +0800

    Zuul网关过滤器

---
+++ springcloud2.0-zuul-apigateway/src/main/java/com/lchtest/filter/TokenFilter.java
@@ -0,0 +1,88 @@
+package com.lchtest.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+/**
+ * 网关服务过滤器
+ * @author pc
+ * @Component注解，将@Component注入到spring容器
+ */
+@Component
+public class TokenFilter extends ZuulFilter{
+	
+	/**
+	 * 过滤器类型
+	 */
+	private static final String FILTE_RTYPE = "pre";
+	
+	/**
+	 * 过滤器是否生效
+	 */
+	private static final boolean SHOULD_FILTER_EFFECTIVE  = true;
+	
+	/**
+	 * 接口鉴权失败
+	 */
+	private static final int UNAUTHORIZED = 401;
+
+	/**
+	 * 判断过滤器是否生效
+	 */
+	public boolean shouldFilter() {
+		return SHOULD_FILTER_EFFECTIVE;
+	}
+
+	/**
+	 * 过滤器业务逻辑
+	 */
+	public Object run() throws ZuulException {
+		//拦截所有服务接口，判断服务接口上是否有传递userToken参数
+		//1.获取上下文
+		RequestContext currentContext = RequestContext.getCurrentContext();
+		
+		//2.获取request对象
+		HttpServletRequest request = currentContext.getRequest();
+		
+		//3.获取token token一般都放在请求头里面发送请求，因此获取token也是从请求头里面获取
+//		String token = request.getHeader("token");
+		// 这里模拟从请求参数中获取token
+		String token = request.getParameter("userToken");
+		
+		if(StringUtils.isEmpty(token)) {
+			// 不往下执行，由网关服务直接响应客户端
+			currentContext.setSendZuulResponse(false);
+			// 返回错误提示
+			currentContext.setResponseBody("userToken is missed.");
+			currentContext.setResponseStatusCode(UNAUTHORIZED);
+			return null;
+		} 
+		
+		// 接口鉴权通过，正常调用其他服务接口
+		return null;
+	}
+
+	/**
+	 * 过滤器类型：
+	 * pre:在请求处理之前执行
+	 */
+	@Override
+	public String filterType() {
+		return FILTE_RTYPE;
+	}
+
+	/**
+	 * 过滤器的执行顺序(优先级)
+	 * 一个请求在同一阶段存在多个过滤器的时候，存在过滤器的执行顺序
+	 */
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
