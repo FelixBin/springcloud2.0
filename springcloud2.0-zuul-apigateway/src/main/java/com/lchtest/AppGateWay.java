d82f19a157caf20dafda61809aa80cc436451103
liuch0228 <1402828263@qq.com>
2020-02-15 10:30:52 +0800

    @EnableZuulProxy注解开启网关代理

--- springcloud2.0-zuul-apigateway/src/main/java/com/lchtest/AppGateWay.java
+++ springcloud2.0-zuul-apigateway/src/main/java/com/lchtest/AppGateWay.java
@@ -3,15 +3,18 @@ package com.lchtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 /**
  * 微服务网关
+ * @EnableZuulProxy 开启网关代理！
  * @author pc
  *
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class AppServiceZuul {
+@EnableZuulProxy
+public class AppGateWay {
 	public static void main(String[] args) {
-		SpringApplication.run(AppServiceZuul.class, args);
+		SpringApplication.run(AppGateWay.class, args);
 	}
 }
