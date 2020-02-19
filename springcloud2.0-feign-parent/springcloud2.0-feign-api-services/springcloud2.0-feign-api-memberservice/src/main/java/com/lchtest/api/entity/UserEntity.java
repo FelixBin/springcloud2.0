fb55e064ed763ee14339aefce7c95f272e1ec416
liuch0228 <1402828263@qq.com>
2020-02-19 11:00:59 +0800

    UserEntity去掉多余注解

--- springcloud2.0-feign-parent/springcloud2.0-feign-api-services/springcloud2.0-feign-api-memberservice/src/main/java/com/lchtest/api/entity/UserEntity.java
+++ springcloud2.0-feign-parent/springcloud2.0-feign-api-services/springcloud2.0-feign-api-memberservice/src/main/java/com/lchtest/api/entity/UserEntity.java
@@ -1,19 +1,12 @@
 package com.lchtest.api.entity;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 /**
  * 
  * @author pc
  *
  */
 @Data
-@Setter
-@Getter
-@ToString
 public class UserEntity {
 
 	private String name;
